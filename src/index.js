f0a57abf07d153f4081a96ac8ebd10e806bf3ee6
AlexxSome <alex.soto.29.9.88@gmail.com>
2024-01-21 23:55:41 -0300

    fix route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/enojada-app/home">
+  <BrowserRouter basename="/enojada-app">
     <App />
   </BrowserRouter>
 );
